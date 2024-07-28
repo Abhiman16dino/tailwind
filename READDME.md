727f9c42dccaa37bfc5573ebf0bcbc259f981227
i526526 <abhishek.mondal@sap.com>
2024-07-28 12:30:13 +0530

    1. Setup     a. Starting vite project     b. setting up tailwind 2. customizing tailwind.cofig.js     a. extending color     b. adding css based on screen size     c. Inline with tailwind class 3. customizing index.css     a. adding custom css @layer     b. adding custom css with tailwind class @apply

---
+++ READDME.md
@@ -0,0 +1,10 @@
+1. Setup
+    a. Starting vite project
+    b. setting up tailwind
+2. customizing tailwind.cofig.js
+    a. extending color
+    b. adding css based on screen size
+    c. Inline with tailwind class
+3. customizing index.css
+    a. adding custom css @layer
+    b. adding custom css with tailwind class @apply
---
+++ projectone/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-yellow-400 sm:text-vuejs md:text-gray-100 lg:text-red-500 bg-green-100 border-4 border-green-950'>
+        Hello World!
+        </h1>
+
+        <h1 className= 'bg-[#bada55]'>
+        Hello World!
+        </h1>
+    </div>
+  )
+}
+
+export default App
